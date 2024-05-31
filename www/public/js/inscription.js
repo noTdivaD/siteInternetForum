0a51e3894d91eb50a8868baef2b7412840bf12b9
lm206097 <marin.lafitte@etu.unice.fr>
2024-05-31 13:23:07 +0200

    Modif Api Liste Pays

--- www/public/js/inscription.js
+++ www/public/js/inscription.js
@@ -1,4 +1,3 @@
-//Détecte une erreur dans l'url et l'affiche dans mon html
 window.addEventListener('load', function() {
     const urlParams = new URLSearchParams(window.location.search);
     const error = urlParams.get('error');
@@ -7,9 +6,22 @@ window.addEventListener('load', function() {
         document.getElementById("error_message").innerText = errorMessage;
         console.log(errorMessage)
     }
+
+    // Génère une liste de Pays via une API alternative
+    const select = document.getElementById('pays');
+
+    fetch('https://countriesnow.space/api/v0.1/countries')
+    .then(response => response.json())
+    .then(data => {
+        data.data.forEach(country => {
+            const option = document.createElement('option');
+            option.value = country.country;
+            option.textContent = country.country;
+            select.appendChild(option);
+        });
+    })
+    .catch(error => console.error('Erreur lors du chargement des pays:', error));
 });
-
-
 
 document.getElementById('registrationForm').addEventListener('submit', function(event) {
     event.preventDefault();  // Prévenir la soumission du formulaire pour la démonstration
@@ -18,39 +30,39 @@ document.getElementById('registrationForm').addEventListener('submit', function(
 
     clearErrors(); // Efface toutes les erreurs précédentes
 
-     // Vérification du nom
-     if (document.getElementById('nom').value.trim().length < 2 || !/^[A-Za-zàâäéèêëïîôöùûüç]+([-'][A-Za-zàâäéèêëïîôöùûüç]+)*$/.test(document.getElementById('nom').value.trim())) {
+    // Vérification du nom
+    if (document.getElementById('nom').value.trim().length < 2 || !/^[A-Za-zàâäéèêëïîôöùûüç]+([-'][A-Za-zàâäéèêëïîôöùûüç]+)*$/.test(document.getElementById('nom').value.trim())) {
         showError('nom', "Le nom n'est pas correct.");
         isValid = false;
-        firstInvalidElement = firstInvalidElement || nom; // Stocke le premier élément invalide
+        firstInvalidElement = firstInvalidElement || document.getElementById('nom'); // Stocke le premier élément invalide
     }
 
     // Vérification du prénom
     if (document.getElementById('prenom').value.trim().length < 2 || !/^[A-Za-zàâäéèêëïîôöùûüç]+([-'][A-Za-zàâäéèêëïîôöùûüç]+)*$/.test(document.getElementById('prenom').value.trim())) {
         showError('prenom', "Le prénom n'est pas correct.");
         isValid = false;
-        firstInvalidElement = firstInvalidElement || prenom;
+        firstInvalidElement = firstInvalidElement || document.getElementById('prenom');
     }
 
     // Vérification de l'adresse
     if (document.getElementById('adresse').value.trim().length < 2 || !/^[A-Za-zàâäéèêëïîôöùûüç0-9]+([ '-][A-Za-zàâäéèêëïîôöùûüç0-9]+)*$/.test(document.getElementById('adresse').value.trim())) {
         showError('adresse', "L'adresse est invalide.");
         isValid = false;
-        firstInvalidElement = firstInvalidElement || adresse;
+        firstInvalidElement = firstInvalidElement || document.getElementById('adresse');
     }
 
     // Vérification de la ville
     if (document.getElementById('ville').value.trim().length < 2 || !/^[A-Za-zàâäéèêëïîôöùûüç]+([-'][A-Za-zàâäéèêëïîôöùûüç]+)*$/.test(document.getElementById('ville').value.trim())) {
         showError('ville', "La ville est invalide.");
         isValid = false;
-        firstInvalidElement = firstInvalidElement || ville;
+        firstInvalidElement = firstInvalidElement || document.getElementById('ville');
     }
 
     // Vérification du code postal
     if (document.getElementById('codePostal').value.trim().length < 2 || !/^[0-9]+$/.test(document.getElementById('codePostal').value.trim())) {
         showError('codePostal', "Code Postal Invalide");
         isValid = false;
-        firstInvalidElement = firstInvalidElement || codePostal;
+        firstInvalidElement = firstInvalidElement || document.getElementById('codePostal');
     }
 
     // Vérification de l'email
@@ -59,15 +71,15 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     if (!emailRegex.test(email)) {
         showError('email', "L'adresse email n'est pas valide.");
         isValid = false;
-        firstInvalidElement = firstInvalidElement || email;
+        firstInvalidElement = firstInvalidElement || document.getElementById('email');
     }
 
-    //Vérification du mot de passe
+    // Vérification du mot de passe
     var password = document.getElementById('password').value;
     if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/)) {
         showError('password', "Format du mot de passe incorrect.");
         isValid = false;
-        firstInvalidElement = firstInvalidElement || password;
+        firstInvalidElement = firstInvalidElement || document.getElementById('password');
     }
 
     // Vérification des mots de passe
@@ -75,9 +87,8 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     if (password !== confirm_password) {
         showError('confirm_password', "Les mots de passe ne correspondent pas.");
         isValid = false;
-        firstInvalidElement = firstInvalidElement || confirm_password;
+        firstInvalidElement = firstInvalidElement || document.getElementById('confirm_password');
     }
-    
 
     if (!isValid) {
         event.preventDefault(); // Empêche la soumission du formulaire si invalide
@@ -85,7 +96,7 @@ document.getElementById('registrationForm').addEventListener('submit', function(
         document.getElementById('error-form').style.display = 'block'; // Affiche le message d'erreur général
         firstInvalidElement.scrollIntoView({ behavior: 'smooth', block: 'center' }); // Défile jusqu'au premier champ invalide
     } else {
-        document.getElementById('error-form').style.display = 'none'; // Affiche le message d'erreur général
+        document.getElementById('error-form').style.display = 'none'; // Cache le message d'erreur général
         this.submit(); // Soumet le formulaire si tout est valide
     }
 });
@@ -112,23 +123,6 @@ function clearErrors() {
     });
     var inputs = document.querySelectorAll('input, select');
     inputs.forEach(function(input) {
-        input.style.borderColor = 'initial'; 
+        input.style.borderColor = 'initial';
     });
 }
-
-//Génère une liste de Pays via une API
-window.addEventListener('load', function() {
-    const select = document.getElementById('pays');
-
-    fetch('https://restcountries.com/v3.1/all') // API pour obtenir des informations sur tous les pays
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(country => {
-            const option = document.createElement('option');
-            option.value = country.name.common; // Utilisez country.cca2 si vous préférez les codes pays
-            option.textContent = country.name.common;
-            select.appendChild(option);
-        });
-    })
-    .catch(error => console.error('Erreur lors du chargement des pays:', error));
-});
